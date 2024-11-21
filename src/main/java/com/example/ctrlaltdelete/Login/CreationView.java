59b6f98b87df9e9825c46c1e98e0bbd602fb7d9d
jlouissaint13 <jaredlouissaint@gmail.com>
2024-11-21 17:44:19 -0500

    Started on login
    Signed-off-by: jlouissaint13 <jaredlouissaint@gmail.com>

--- src/main/java/com/example/ctrlaltdelete/Login/CreationView.java
+++ src/main/java/com/example/ctrlaltdelete/Login/CreationView.java
@@ -5,7 +5,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -14,29 +17,81 @@ import javafx.stage.Stage;
 public class CreationView {
     private Stage stage;
     public Scene scene;
-    private GridPane gridPane;
+    private GridPane gridPaneR,gridPaneL;
     private Label fName,lName,email,pw,phoneNumber;
-    private TextField createAccount,fNameE,lNameE,emailE,passwordE,phoneNumberE;
+    private Text createAccount;
+    private TextField fNameE,lNameE,emailE,passwordE,phoneNumberE;
     private Button cAccount;
+        //All variables with e at the end means that it is the variable for the textfield;
     public void components() {
-
+        createAccount = new Text("Create Account");
+        fName = new Label("First Name*");
+        fNameE = new TextField();
+        lName = new Label("Last Name*");
+        lNameE = new TextField();
+        email = new Label("Email*");
+        emailE = new TextField();
+        pw = new Label("Password*");
+        passwordE = new TextField();
+        phoneNumber = new Label("Phone Number*");
+        phoneNumberE = new TextField();
+        cAccount = new Button("Create Account");
     }
     public void properties() {
+        fNameE.setMaxWidth(100);
+        lNameE.setMaxWidth(100);
+        emailE.setMaxWidth(200);
 
+        //gridpane properties
+        gridPaneL.setAlignment(Pos.TOP_LEFT);
+        gridPaneL.setHgap(10);
+        gridPaneL.setVgap(5);
+        gridPaneR.setHgap(10);
+        gridPaneR.setVgap(5);
+        gridPaneR.setAlignment(Pos.TOP_RIGHT);
     }
     public void addComponents() {
+
+
+
+
+        //gridpaneL
+        gridPaneL.add(fName,3,23);
+        gridPaneL.add(fNameE,3,24);
+        gridPaneL.add(email,3,40);
+        gridPaneL.add(emailE,3,41);
+        //gripdPaneR
+
+        gridPaneR.add(lName,9,23);
+        gridPaneR.add(lNameE,9,24);
+
+
+    }
+    public void initializeGridPane() {
+        gridPaneL = new GridPane();
+        gridPaneR = new GridPane();
+        ColumnConstraints columnConstraints = new ColumnConstraints();
+        RowConstraints rowConstraints = new RowConstraints();
+        columnConstraints.setPercentWidth(0);
+        gridPaneL.getColumnConstraints().add(columnConstraints);
+        gridPaneL.getRowConstraints().add(rowConstraints);
+        gridPaneR.getRowConstraints().add(rowConstraints);
+        gridPaneR.getColumnConstraints().add(columnConstraints);
+
+
 
     }
 
     public CreationView(Stage stage) {
-        gridPane = new GridPane(350,600);
-        scene = new Scene(gridPane,350,600);
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setHgap(10);
-        gridPane.setVgap(5);
+        components();
+        initializeGridPane();
+        properties();
+        addComponents();
+        HBox hbox = new HBox(gridPaneL,gridPaneR);
+        scene = new Scene(hbox,350,600);
         stage.setTitle("Create Account");
         stage.setScene(scene);
-        gridPane.add(new Text("Testing"),0,0);
+
 
     }
 
