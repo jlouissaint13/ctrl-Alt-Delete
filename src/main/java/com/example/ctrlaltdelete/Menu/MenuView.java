a313777045aa913075502e2520845e27288f0f49
jlouissaint13 <jaredlouissaint@gmail.com>
2024-11-29 23:03:15 -0500

    N/A

--- src/main/java/com/example/ctrlaltdelete/Menu/MenuView.java
+++ src/main/java/com/example/ctrlaltdelete/Menu/MenuView.java
@@ -15,11 +15,18 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 public class MenuView {
-    private Text word;
+    private Pane pane;
+    private Text word,wordTwo;
+    private Scene scene;
+    private Button minus,plus;
+    private TextField special;
     //Here you will initialize your components
 public void components() {
     //word = new word("Word");
-    //pane = new Pane
+    pane = new Pane();
+    //wordTwo = new Text("30");
+    //special = new TextField();
+    //minus = new Button("-");
 }
 
 //Decide the properties size and where they go
@@ -27,10 +34,23 @@ public void properties() {
     //word.setLayoutX();
     //word.setLayoutY();
 
-}
 
+}
+//This one is for adding elements to the pane
 public void addComponents() {
     //pane.getChildren.add(word);
+
+}
+
+public MenuView(Stage stage) {
+   components();
+   properties();
+   addComponents();
+
+    VBox vBox = new VBox(pane);
+    scene = new Scene(vBox,350,600);
+    stage.setTitle("Menu Page");
+    stage.setScene(scene);
 }
 
 }
