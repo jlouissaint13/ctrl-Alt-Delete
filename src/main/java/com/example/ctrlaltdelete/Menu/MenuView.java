8e43826f05a56c7866f8224c719f053e065f7a0c
jlouissaint13 <jaredlouissaint@gmail.com>
2024-11-29 20:45:39 -0500

    N/A

---
+++ src/main/java/com/example/ctrlaltdelete/Menu/MenuView.java
@@ -0,0 +1,36 @@
+package com.example.ctrlaltdelete.Menu;
+import javafx.animation.PauseTransition;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+public class MenuView {
+    private Text word;
+    //Here you will initialize your components
+public void components() {
+    //word = new word("Word");
+    //pane = new Pane
+}
+
+//Decide the properties size and where they go
+public void properties() {
+    //word.setLayoutX();
+    //word.setLayoutY();
+
+}
+
+public void addComponents() {
+    //pane.getChildren.add(word);
+}
+
+}
